99eaefa2bffaaf5d94390f1d59c4871f81a936f9
Paritosh Chaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-10-22 22:46:21 +0530

    Create 31_sort_colors.cpp

---
+++ 31_sort_colors.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/sort-colors/description/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size() ;
+
+        int zero = 0 , one = 0 , two = 0 ;
+
+        for(int i = 0 ; i < n ; ++i)
+        {
+            if(nums[i] == 0)zero++ ;
+            else if(nums[i] == 1)one++ ;
+            else two++ ;
+        }
+
+        int i = 0 ;
+
+        while(zero--)
+        {
+            nums[i] = 0 ;
+            i++ ;
+        }
+
+        while(one--)
+        {
+            nums[i] = 1 ;
+            i++ ;
+        }
+
+        while(two--)
+        {
+            nums[i] = 2 ;
+            i++ ;
+        }
+    }
+};
