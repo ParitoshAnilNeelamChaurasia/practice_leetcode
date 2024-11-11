f1a44a32e3b725780108bca042b3bdea0b27c1e2
Paritosh Chaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-11-12 03:52:40 +0530

    Create 45_can_place_flowers.cpp

---
+++ 45_can_place_flowers.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int m = flowerbed.size() ;
+
+        if(n == 0)
+        {
+            return true ;
+        }
+
+        for(int i = 0 ; i < m ; ++i)
+        {
+            bool left_empty = false ;
+            bool right_empty = false ;
+
+            if(i == 0 || flowerbed[i-1] == 0)
+            {
+                left_empty = true ;
+            }
+
+            if(i == m-1 || flowerbed[i+1] == 0)
+            {
+                right_empty = true ;
+            }
+
+            if(flowerbed[i] == 0 && left_empty == true && right_empty == true)
+            {
+                flowerbed[i] = 1 ;
+                n-- ;
+                if(n == 0)
+                return true ;
+            }
+        }
+        return n==0 ;
+    }
+};
